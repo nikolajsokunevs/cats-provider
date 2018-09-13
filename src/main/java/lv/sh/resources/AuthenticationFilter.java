2424331a3e6e4086fdc57e29f8fc5617c3800022
Nikolay Okunev <nikolay.okunev@ctco.lv>
2018-09-13 15:13:36 +0300

    ok

--- src/main/java/lv/sh/resources/AuthenticationFilter.java
+++ src/main/java/lv/sh/resources/AuthenticationFilter.java
@@ -85,13 +85,13 @@ public class AuthenticationFilter implements javax.ws.rs.container.ContainerRequ
     private boolean isUserAllowed(final String username, final String password, final Set<String> rolesSet) {
         boolean isAllowed = false;
 
-        if (username.equals("user") && password.equals("scumgang")) {
+        if (username.equals("user") && password.equals("scrumgang")) {
             String userRole = "USER";
 
             if (rolesSet.contains(userRole)) {
                 isAllowed = true;
             }
-        } else if (username.equals("admin") && password.equals("scumgang")) {
+        } else if (username.equals("admin") && password.equals("scrumgang")) {
             String userRole = "ADMIN";
 
             if (rolesSet.contains(userRole)) {
